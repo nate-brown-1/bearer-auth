edf7d85e7287acfc053bf226ac429197f98364c6
Nate Brown <nathan.d.brown@gmail.com>
2023-04-11 23:01:01 -0700

    first bugs fixed, server is able to listen, 4/18 tests pass

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 
 // Start up DB Server
 const { db } = require('./src/auth/models/index.js');
+
+const { startup } = require('./src/server.js');
+
+const PORT = process.env.PORT;
+
 db.sync()
   .then(() => {
 
     // Start the web server
-    require('./src/server.js').start(process.env.PORT);
+    // require('./src/server.js').start(process.env.PORT);
+    startup(PORT);
   });
 
--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 const userSchema = require('./users.js');
 
-const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DATABASE_URL;
+// const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DATABASE_URL;
+
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
 
 const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
   dialectOptions: {
